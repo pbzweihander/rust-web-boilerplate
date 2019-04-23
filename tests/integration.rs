e7796f4241f593b4cd7fda6a2efb9c7d64375ed5
rusty <rusty@spoqa.com>
2019-04-23 21:53:37 +0900

    Split integration tests by endpoints

--- tests/integration.rs
+++ tests/integration.rs
@@ -7,7 +7,7 @@ use {
 };
 
 #[test]
-fn integration_test() {
+fn test_not_found() {
     let opt = Opt {
         host: "0.0.0.0".to_string(),
         port: 5000,
@@ -19,6 +19,17 @@ fn integration_test() {
     let req = Request::get("/foobar").body(Body::empty()).unwrap();
     let resp = server.simulate(req).unwrap();
     assert_eq!(resp.status(), 404);
+}
+
+#[test]
+fn test_ping() {
+    let opt = Opt {
+        host: "0.0.0.0".to_string(),
+        port: 5000,
+    };
+    let app = make_app(opt);
+
+    let mut server = make_server(app.into_http_service()).unwrap();
 
     let req = Request::get("/ping").body(Body::empty()).unwrap();
     let resp = server.simulate(req).unwrap();
@@ -26,6 +37,17 @@ fn integration_test() {
     let body = block_on(resp.into_body().into_vec()).unwrap();
     let body_str = String::from_utf8(body).unwrap();
     assert_eq!(&body_str, "OK");
+}
+
+#[test]
+fn test_hello() {
+    let opt = Opt {
+        host: "0.0.0.0".to_string(),
+        port: 5000,
+    };
+    let app = make_app(opt);
+
+    let mut server = make_server(app.into_http_service()).unwrap();
 
     let req = Request::get("/hello/foobar").body(Body::empty()).unwrap();
     let resp = server.simulate(req).unwrap();
@@ -33,6 +55,17 @@ fn integration_test() {
     let body = block_on(resp.into_body().into_vec()).unwrap();
     let body_str = String::from_utf8(body).unwrap();
     assert_eq!(&body_str, "Hello, foobar!");
+}
+
+#[test]
+fn test_hello_with_body() {
+    let opt = Opt {
+        host: "0.0.0.0".to_string(),
+        port: 5000,
+    };
+    let app = make_app(opt);
+
+    let mut server = make_server(app.into_http_service()).unwrap();
 
     let req = Request::post("/hello/foobar").body("baz".into()).unwrap();
     let resp = server.simulate(req).unwrap();
